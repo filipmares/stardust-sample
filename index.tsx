a51674fc22abba9ee438f97523ac6351eaff70c0
fimares <fimares@microsoft.com>
2018-11-19 15:40:14 -0800

    moved app tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider, themes } from '@stardust-ui/react'
 
-import App from './App'
+import App from './src/App'
 
 ReactDOM.render(
   <Provider theme={themes.teams}>
