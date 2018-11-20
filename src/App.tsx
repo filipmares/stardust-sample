9596fd8288d63fad86eeaa8c9ec0f3bed7d61b18
fimares <fimares@microsoft.com>
2018-11-20 12:08:16 -0800

    chat sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,32 @@
 import React from 'react'
-import { Button } from '@stardust-ui/react'
+import { Button, Header, Input, Text, Layout, Chat } from '@stardust-ui/react'
 
 export default () => (
-  <Button content="Hello World" icon="arrow right" iconPosition="after" primary />
+    <Layout vertical>
+        <Header as="h1" content="Hello World Chart" />
+        <Chat>
+            <Chat.Item>
+                <Chat.Message content="Hello" author="John Doe" timestamp="Yesterday, 10:15 PM" mine />
+            </Chat.Item>
+            <Chat.Item>
+                <Chat.Message
+                    content="Hi"
+                    author="Jane Doe"
+                    timestamp="Yesterday, 10:15 PM"
+                />
+            </Chat.Item>
+            <Chat.Item>
+                <Chat.Message
+                    content="Would you like to grab a lunch?"
+                    author="John Doe"
+                    timestamp="Yesterday, 10:16 PM"
+                    mine
+                />
+            </Chat.Item>
+        </Chat>
+        <Layout>
+            <Input placeholder="Type a message here" />
+            <Button content="Send" primary />
+        </Layout>
+    </Layout>
 )
